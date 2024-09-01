af816b5ef0fb0f97e90fbd8d45bd51b628289c18
surajsahu01 <surajsahu6365@gmail.com>
2024-09-02 03:21:52 +0530

    Add New code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
       },
       colors: {
         deepBlue: "#02042a",
-        lightBlue: "2b84ea",
-        lightBlue300: "4b94ed",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
         lightBlue500: "#0b72e7",
         greenLight: "#61cea6",
         grayText: "#818597",
